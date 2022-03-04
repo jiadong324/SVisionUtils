6cd2e1616a054cd5b5797ccaf453dbaf35cd00dd
Jiadong324 <jdlin910324@gmail.com>
2022-03-04 09:02:24 +0800

    Add Supports

--- Configs.py
+++ Configs.py
@@ -17,8 +17,8 @@ BEDTOOLS = '/Users/jiadonglin/miniconda3/envs/dnatools/bin/bedtools'
 
 AUTOSOMES = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12",
                   "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"]
-RMSK = '/Users/jiadonglin/Data/genome/repeat_annot/hg38.rmsk.bed'
-TRF = '/Users/jiadonglin/Data/genome/repeat_annot/hg38.trf.bed'
-EXCLUDE = "/Users/jiadonglin/Data/genome/grch38.exclude_regions_cen.bed"
-REF = "/Users/jiadonglin/Data/genome/GRCh38_full_analysis_set_plus_decoy_hla.fa"
+RMSK = '/Users/apple/Data/genome/repeat_annot/grch38/hg38.rmsk.bed'
+TRF = '/Users/apple/Data/genome/repeat_annot/grch38/hg38.trf.bed'
+EXCLUDE = "/Users/apple/Data/genome/repeat_annot/grch38/grch38.exclude_regions_cen.bed"
+REF = "/Users/apple/Data/genome/GRCh38_full_analysis_set_plus_decoy_hla.fa"
 
